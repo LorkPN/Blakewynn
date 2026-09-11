6d4ec23e94114bf4c54516b52b032c35d49ac8c2
Matt Young <lorkhb@gmail.com>
2026-09-11 00:21:07 -0400

    Added gear type back and removed gear effect type.

---
+++ Assets/_Project/Scripts/Data/Enums/GearType.cs
@@ -0,0 +1,23 @@
+namespace _Project.Scripts.Data.Enums
+{
+    public enum GearType
+    {
+        Sword,
+        Mace,
+        Spear,
+        Axe,
+        Dagger,
+        ShortBow,
+        LongBow,
+        Staff,
+        Wand,
+        Breastplate,
+        Helmet,
+        Leggings,
+        Belt,
+        Cloak,
+        Necklace,
+        Boots,
+        Offhand
+    }
+}
